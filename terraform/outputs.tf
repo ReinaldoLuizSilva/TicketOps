8489f62e06fd5ac316e1538feaeb8d976608b094
ReinaldoLuizSilva <reinaldoluisdasilva@gmail.com>
2026-08-19 16:43:33 -0300

    feat: adicionar serviço Cloud Run, acesso público e outputs

---
+++ terraform/outputs.tf
@@ -0,0 +1,14 @@
+output "service_url" {
+  description = "URL do serviço Cloud Run"
+  value       = google_cloud_run_v2_service.api.uri
+}
+
+output "image_repo" {
+  value       = "${var.region}-docker.pkg.dev/${var.project_id}/${google_artifact_registry_repository.docker.repository_id}"
+  description = "URL do repositório de imagens do Artifact Registry"
+}
+
+output "runtime_service_account" {
+  value       = google_service_account.run.email
+  description = "E-mail da Service Account usada pelo serviço Cloud Run"
+}
