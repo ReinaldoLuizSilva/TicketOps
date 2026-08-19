b2b7218252e1ec511f4ab85c2f366897577678cb
Reinaldo Luiz da Silva <142761243+ReinaldoLuizSilva@users.noreply.github.com>
2026-08-19 17:26:53 -0300

    feat: adicionar base do Terraform e habilitar APIs do GCP (#3)

---
+++ terraform/apis.tf
@@ -0,0 +1,16 @@
+locals {
+  services = [
+    "cloudresourcemanager.googleapis.com",
+    "iam.googleapis.com",
+    "run.googleapis.com",
+    "artifactregistry.googleapis.com",
+    "secretmanager.googleapis.com",
+  ]
+}
+
+resource "google_project_service" "enable" {
+  for_each = toset(local.services)
+  service  = each.value
+
+  disable_on_destroy = false
+}
