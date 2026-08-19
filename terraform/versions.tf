614937b1aa1d44abe7900391d070f45197d68172
ReinaldoLuizSilva <reinaldoluisdasilva@gmail.com>
2026-08-19 14:41:06 -0300

    feat: adicionar base do Terraform e habilitar APIs do GCP

---
+++ terraform/versions.tf
@@ -0,0 +1,15 @@
+terraform {
+  required_version = ">= 1.9"
+
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "~> 6.0"
+    }
+  }
+
+  backend "gcs" {
+    bucket = "ticketops-tfstate-63450"
+    prefix = "ticketops/state"
+  }
+}
