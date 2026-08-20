3afff48019103244b288fd5e9ead35e2bf3518bc
Reinaldo Luiz da Silva <142761243+ReinaldoLuizSilva@users.noreply.github.com>
2026-08-20 15:28:43 -0300

    fix: adicionar o client e client_version para registro quem foi o último a modificá-lo (#14)

--- terraform/run.tf
+++ terraform/run.tf
@@ -45,6 +45,8 @@ resource "google_cloud_run_v2_service" "api" {
   lifecycle {
     ignore_changes = [
       template[0].containers[0].image,
+      client,
+      client_version,
       scaling,
     ]
   }
